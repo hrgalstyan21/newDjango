914dae5c2da0b2bcaba0bcca6fc09678e0f83a19
Hrayr <hrgalstyan06@gmail.com>
2024-08-30 17:21:15 +0400

    30/08/01

--- firstApp/main/views.py
+++ firstApp/main/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'title': 'Home Page',
+        'values': ['Some', 'Home', '1234'],
+        'obj': {
+            'car': 'BMW',
+            'age': 18,
+            'hobby': 'Football'
+        }
+    }
+    return render(request, 'main/index.html', data)
 
 def about(request):
     return render(request, 'main/about.html')
